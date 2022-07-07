7dd0ffdb83881f4b66aac4047414dc867304db1d
greg-strom <greg.b.strom@gmail.com>
2022-07-06 23:04:35 -0400

    changed "App" to "MainView" and added imports

---
+++ src/components/main-view/main-view.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../../logo.svg';
+
+import { LoginView } from '../login-view/login-view';
+//import { MovieCard } from '../movie-card/movie-card';
+//import { MovieView } from '../movie-view/movie-view';
+//import { DirectorView } from '../director-view/director-view';
+//import { GenreView } from '../genre-view/genre-view';
+//import { RegistrationView } from '../registration-view/registration-view';
+//import { ProfileView } from '../profile-view/profile-view';
+//import { UpdateView } from '../update-view/update-view';
+
+import './main-view.css';
+
+function MainView() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default MainView;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Container from 'react-bootstrap/Container';
 import './index.css';
-import App from './App';
+import MainView from './components/main-view/main-view';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Container>
-      <App />
+      <MainView />
     </Container>
   </React.StrictMode>
 );
